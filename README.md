cbd1bf2e55d9c15bdb43f8d41a50e531046e73b3
Frank Falor <ffalorjr@outlook.com>
2019-04-05 14:52:05 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+
+# groupmembers
+
+#### Table of Contents
+
+1. [Description](#description)
+2. [Requirements](#Requirements)
+3. [Usage - Configuration options and additional functionality](#usage)
+    * [Puppet Tasks and Bolt](#Puppet-Task-and-Bolt)
+    * [Puppet Task API](#Puppet-Task-Api)
+4. [Development - Guide for contributing to the module](#development)
+
+## Description
+
+This module includes a puppet task to help manage local groups.
+
+This task can be used to remove or add members to local security groups. Allowing administrators and customers to add and remove multiple users to multiple machines.
+
+This task can be exposed as a service via the puppet task endpoint to allow remote execution and self service access management.
+
+
+## Requirements
+
+This module is compatible with Puppet Enterprise and Puppet Bolt.
+
+- To run tasks with Puppet Enterprise, PE 2018.1 or later must be installed on the machine from which you are running task commands. Machines receiving task requests must be Puppet agents.
+- To run tasks with Puppet Bolt, Bolt 1.0 or later must be installed on the machine from which you are running task commands. Machines receiving task requests must have SSH or WinRM services enabled.
+
+
+## Usage
+
+### Puppet Task and Bolt
+To run an groupmembers task, use the task command, specifying the command to be executed.
+
+* With PE on the command line, run `puppet task run groupmembers ensure=<present|absent> group=<groupname> member=<String|Array>`.
+* With Bolt on the command line, run `bolt task run groupmembers ensure=<present|absent> group=<groupname> member=<String|Array>`.
+
+For example, to add a example\jdoe to administrators group, run:
+
+* With PE, run `puppet task run groupmembers ensure=present group=administrators member="example\\jdoe" --nodes saturn`.
+* With Bolt, run `bolt task run groupmembers ensure=present group=administrators member="example\\jdoe" --nodes saturn`.
+
+### Puppet Task API
+
+endpoint: `https://<puppet>:8143/orchestrator/v1/command/task`
+
+method: `post`
+
+body:
+```json
+{
+    "environment": "production",
+    "task": "groupmembers",
+    "params": {
+        "ensure": "present",
+        "group": "Administrators",
+        "member": [
+            "example\\jdoe",
+            "example\\dotterman"
+        ]
+    },
+    "description": "Description for task",
+    "scope": {
+        "nodes": [
+            "saturn.example.com"
+        ]
+    }
+}
+```
+
+You can also run tasks in the PE console. See PE task documentation for complete information.
+
+## Limitations
+
+Only tested on 2016 and 2012 R2 window servers.
+
+## Development
+
+Feel free to fork it fix my crappy code and create a PR  (:
---
+++ tasks/windows.ps1
@@ -0,0 +1,170 @@
+# groupmembers: 
+#
+# This task will add or remove users from a local group on the server.
+# Only one group can be managed per task, but any number of members can be managed.
+#
+# The task will return the state of the local group after a successful run, and
+# will error out with proper messages when invalid parameters were passed.
+#
+# Passing in a member that already exists in the group will not result in an error, and is idempotent.
+#
+# Parameters
+# ----------
+#
+# * `ensure`
+#   ENUM['present','absent']
+#   When set to absent user accounts specified in the members parameter are removed.
+#   When set to present user accounts specified in the members parameter are added.
+#
+# * `group`
+#   String[1]
+#   Group represents a local group. Ex: Administrators
+#
+# * `member`
+#   Variant[String[1],Array[String[1]]]
+#   Member represents the users to add or remove from the local group.
+#
+# @example Bolt run from Command Line
+#   bolt task run groupmembers -n localhost 
+#     --params '{
+#      "ensure": "present", 
+#      "group": "Administrators",
+#      "member": [ "domain\\jdoe", "domain\\ffalor" 
+#      }'
+#
+# Authors
+# -------
+#
+# * Falor, Frank    <ffalorjr@outlook.com>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    $ensure,
+
+    [Parameter(Mandatory = $True)]
+    $group,
+
+    [Parameter(Mandatory = $True)]
+    $member
+)
+
+function write_error($ensure, $message, $group, $use_ps, $force) {
+    if (!($force)) {
+        $members = get_members -group $group -use_ps $use_ps
+    }
+    else {
+        $members = "Unable to get members of group."
+    }
+
+    if (!([String]::IsNullOrWhiteSpace($message))) {
+        $message = $message | ConvertTo-Json
+    }
+    else {
+        $message = ""
+    }
+
+
+    $error_payload = @"
+{
+    "_error": {
+        "msg": $message,
+        "kind": "puppetlabs.tasks/task-error",
+        "details": {
+            "group": "${group}",
+            "members": ${members},
+            "exitcode": 1
+        }
+    },
+    "_output": "Something went wrong with task",
+    "ensure": "${ensure}",
+    "group": "${group}",
+    "members": ${members}
+}
+"@
+
+    Write-Host $error_payload | ConvertTo-Json -Compress
+}
+
+function write_success($ensure, $group, $use_ps) {
+    try {
+
+        $members = get_members -group $group -use_ps $use_ps
+        $success_payload = @"
+{
+        "ensure": ${ensure},
+        "group": "${group}",
+        "members": ${members}
+}
+"@
+        Write-Host $success_payload
+
+    }
+    catch {
+        $error_message = $_.Exception.Message
+        write_error -ensure $ensure -message $error_message -group $group -use_ps $False -force $False
+        exit 1
+    }
+}
+
+function get_members($group, $use_ps) {
+    try {
+        if ($use_ps) {
+            $members = Get-LocalGroupMember -Group $group -ErrorAction Stop
+            return ConvertTo-Json -InputObject $members.name -Compress
+        }
+        else {
+            $cmd = "net.exe localgroup `"${group}`""
+            $result = cmd.exe /c $cmd
+            if ($result.length -ge 1) {
+                $result = $result[6..($result.length - 3)]
+                return ConvertTo-Json -InputObject $result -Compress
+            }
+            else {
+                return  '"No members returned."'
+            }
+        }
+
+    }
+    catch {
+        $error_message = $_.Exception.Message
+        write_error -ensure $ensure -message $error_message -group $group -use_ps $False -force $True
+        exit 1
+    }
+}
+
+$redirect = "2>&1"
+try {
+    if ($ensure -eq "present") {
+        $command_error = New-Object System.Text.StringBuilder
+        foreach ( $m in $member) {
+            $cmd = "net.exe localgroup `"${group}`" `"${m}`" /add"
+            $temp_output = cmd.exe /c $cmd $redirect
+            if ($LASTEXITCODE -ne 0 -and $temp_output[2] -ne "The specified account name is already a member of the group.") {
+                $command_error.Append($temp_output[0]) | Out-Null
+                $command_error.Append(" " + $temp_output[2]) | Out-Null
+                write_error -ensure $ensure -message $command_error.ToString() -group $group -use_ps $False -force $False
+                exit 1
+            }
+        }
+        write_success -ensure $ensure -group $group -use_ps $False  
+    }
+    elseif ($ensure -eq "absent") {
+        $command_error = New-Object System.Text.StringBuilder
+        foreach ( $m in $member) {
+            $cmd = "net.exe localgroup `"${group}`" `"${m}`" /delete"
+            $temp_output = cmd.exe /c $cmd $redirect
+            if ($LASTEXITCODE -ne 0 -and $temp_output[2] -ne "The specified account name is not a member of the group.") {
+                $command_error.Append($temp_output[0]) | Out-Null
+                $command_error.Append(" " + $temp_output[2]) | Out-Null
+                write_error -ensure $ensure -message $command_error.ToString() -group $group -use_ps $False -force $False
+                exit 1
+            }
+        }
+        write_success -ensure $ensure -group $group -use_ps $False
+    }
+}
+catch {
+    $error_message = $_.Exception.Message
+    write_error -ensure $ensure -message $error_message -group $group -use_ps $False -force $False
+    exit 1
+}
